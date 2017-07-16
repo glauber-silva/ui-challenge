0c9450204727923c3cec25bebc25c4fcfa391471
Glauber Silva <glauber.lucio.silva@gmail.com>
2017-07-16 01:12:29 -0300

    - webpack.config.js created

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+'user strict'
+
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }
+}
