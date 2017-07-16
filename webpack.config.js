c28c0faff8c075482e7108a2a52b3d9da1a0c491
Glauber Silva <glauber.lucio.silva@gmail.com>
2017-07-16 01:24:22 -0300

    - webpack-dev-server configured

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,11 @@ module.exports = {
     entry: path.join(__dirname, 'src', 'index'),
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+    },
+    devServer: {
+        inline: true,
+        contentBase: './public',
+        port:3000
     }
 }
